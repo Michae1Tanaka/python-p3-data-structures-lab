dcc2b7b3d60fa723bbeccb363baa8b722864fce7
Michael Tanaka <tanakam4214@gmail.com>
2023-07-09 19:35:44 -0700

    Lab Completed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,5 @@
+from operator import attrgetter
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -16,23 +18,42 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    food_name = [foods.get("name") for foods in spicy_foods]
+    return food_name
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    sorted_spicy_foods = [food for food in spicy_foods if food["heat_level"] > 5]
+    return sorted_spicy_foods
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    emoji = "🌶"
+    for food in spicy_foods:
+        print(
+            f"{food['name']} ({food['cuisine']}) | Heat Level: {emoji * food['heat_level']}"
+        )
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if cuisine == food["cuisine"]:
+            return food
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    spiciness_levels = [foods.get("heat_level") for foods in spicy_foods]
+    average = sum(spiciness_levels) // len(spiciness_levels)
+    return average
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
